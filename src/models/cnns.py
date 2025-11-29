5339df4d6084ef00c647e7c41b8ac564d337c850
STH1 <simon.teveshumme@gmail.com>
2025-11-29 23:46:17 +0100

    add new models

--- src/models/cnns.py
+++ src/models/cnns.py
@@ -2,7 +2,9 @@ import torch.nn as nn
 from torchvision.models import (
     mobilenet_v2,
     efficientnet_b0,
-    alexnet
+    alexnet,
+    resnet101,
+    densenet201
 )
 
 def get_mobilenet_v2(pretrained=False, num_classes=1000):
@@ -23,4 +25,16 @@ def get_alexnet(pretrained=False, num_classes=1000):
     model = alexnet(weights="IMAGENET1K_V1" if pretrained else None)
     if num_classes != 1000:
         model.classifier[6] = nn.Linear(4096, num_classes)
+    return model
+
+def get_resnet101(pretrained=False, num_classes=1000):
+    model = resnet101(weights="IMAGENET1K_V2" if pretrained else None)
+    if num_classes != 1000:
+        model.fc = nn.Linear(model.fc.in_features, num_classes)
+    return model
+
+def get_densenet201(pretrained=False, num_classes=1000):
+    model = densenet201(weights="IMAGENET1K_V1" if pretrained else None)
+    if num_classes != 1000:
+        model.classifier = nn.Linear(model.classifier.in_features, num_classes)
     return model
--- src/models/llms.py
+++ src/models/llms.py
@@ -21,3 +21,14 @@ def get_tinyllama():
     model = AutoModelForCausalLM.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     return model, tokenizer
+
+def get_gptneo_27b():
+    model_name = "EleutherAI/gpt-neo-2.7B"
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    
+    # GPT-Neo hat keinen pad_token → nötig für generation()
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+
+    return model, tokenizer
