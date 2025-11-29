03cf4d886107fb9dd5a01ae33fc5c33a2a8f8c8c
STH1 <simon.teveshumme@gmail.com>
2025-11-30 00:08:22 +0100

    removed unnecessary import

--- src/utils/measure_energy.py
+++ src/utils/measure_energy.py
@@ -1,7 +1,6 @@
 import time
 import torch
 import subprocess
-import pyRAPL
 
 def measure_gpu_energy(model_fn, *args, **kwargs):
     start_time = time.time()
@@ -25,25 +24,21 @@ import time
 import psutil
 
 def measure_cpu_energy(model_fn, *args, **kwargs):
-    # CPU-Leistungsaufnahme pro Kern in Watt schätzen (z.B. TDP / Anzahl Kerne)
-    tdp_total_watt = 65  # Gesamt-TDP deiner CPU
+    tdp_total_watt = 65
     num_cores = psutil.cpu_count(logical=False)
     watt_per_core = tdp_total_watt / num_cores
 
     start_time = time.time()
 
-    # Starte Messung der CPU-Auslastung
     cpu_percent_before = psutil.cpu_percent(interval=None)
 
     output = model_fn(*args, **kwargs)
 
     elapsed = time.time() - start_time
 
-    # Durchschnittliche CPU-Auslastung während der Ausführung
     cpu_percent_after = psutil.cpu_percent(interval=None)
-    avg_cpu_load = (cpu_percent_before + cpu_percent_after) / 2 / 100  # in [0,1]
+    avg_cpu_load = (cpu_percent_before + cpu_percent_after) / 2 / 100
 
-    # Energie = Leistung * Zeit
     avg_power_watt = watt_per_core * num_cores * avg_cpu_load
     energy_joule = avg_power_watt * elapsed
 
